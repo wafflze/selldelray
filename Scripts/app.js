837e69791aeb140b4e730edc254f67c47a21ff65
wafflze <daniel_negron@ncci.com>
2015-04-19 16:37:46 -0400

    added csv textarea for work

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,52 +1,56 @@
-(function () {
-'use strict';
+(function() {
+    'use strict';
+
     function HeaderController($scope, $location) {
-        $scope.isActive = function (viewLocation) {
+        $scope.isActive = function(viewLocation) {
             return viewLocation === $location.path();
         };
     };
 
+    function FirstSearchController($scope) {
+        $scope.searchTitle = "Search Delray Beach Real Estate";
+    };
+
     angular
         .module("app", ['ngRoute'])
-        .config(function ($routeProvider) 
-                {
-                    $routeProvider.
-                    when('/home', {
-                        templateUrl: 'scripts/home/home.html',
-                        controller: ('HomeController', HomeController)
-                    }).
-                    when('/search', {
-                        templateUrl: 'scripts/search/search.html',
-                        controller: ('SearchController', SearchController)
-                    }).
-                    when('/selling', {
-                        templateUrl: 'scripts/selling/selling.html',
-                        controller: ('SellingController', SellingController)
-                    }).
-                    when('/buying', {
-                        templateUrl: 'scripts/buying/buying.html',
-                        controller: ('BuyingController', BuyingController)
-                    }).
-                    when('/featured', {
-                        templateUrl: 'scripts/featured/featured.html',
-                        controller: ('FeaturedController', FeaturedController)
-                    }).
-                    when('/about', {
-                        templateUrl: 'scripts/about/about.html',
-                        controller: ('AboutController', AboutController)
-                    }).
-                    when('/blog', {
-                        templateUrl: 'scripts/blog/blog.html',
-                        controller: ('BlogController', BlogController)
-                    }).
-                    when('/contact', {
-                        templateUrl: 'scripts/contact/contact.html',
-                        controller: ('ContactController', ContactController)
-                    }).
-                    otherwise({
-                        redirectTo: 'home'
-                    });
-                    }
-               ).controller("HeaderController", HeaderController);
-
+        .config(function($routeProvider) {
+            $routeProvider.
+            when('/home', {
+                templateUrl: 'scripts/home/home.html',
+                controller: ('HomeController', HomeController)
+            }).
+            when('/search', {
+                templateUrl: 'scripts/search/search.html',
+                controller: ('SearchController', SearchController)
+            }).
+            when('/selling', {
+                templateUrl: 'scripts/selling/selling.html',
+                controller: ('SellingController', SellingController)
+            }).
+            when('/buying', {
+                templateUrl: 'scripts/buying/buying.html',
+                controller: ('BuyingController', BuyingController)
+            }).
+            when('/featured', {
+                templateUrl: 'scripts/featured/featured.html',
+                controller: ('FeaturedController', FeaturedController)
+            }).
+            when('/about', {
+                templateUrl: 'scripts/about/about.html',
+                controller: ('AboutController', AboutController)
+            }).
+            when('/blog', {
+                templateUrl: 'scripts/blog/blog.html',
+                controller: ('BlogController', BlogController)
+            }).
+            when('/contact', {
+                templateUrl: 'scripts/contact/contact.html',
+                controller: ('ContactController', ContactController)
+            }).
+            otherwise({
+                redirectTo: 'home'
+            });
+        }).controller("HeaderController", HeaderController)
+        .controller("FirstSearchController", FirstSearchController);
+    //test
 })();
